3a03d2fb26f6ad00c1709ea43ff7c14780e0a66c
ppate343 <71097041+ppate343@users.noreply.github.com>
2023-06-07 16:00:00 -0400

    garbbing access and fresh tokens from url params

---
+++ client/src/App.js
@@ -0,0 +1,46 @@
+import { useEffect } from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  useEffect(() => {
+
+    //grabbing window location search value (url parameters)
+    const queryString = window.location.search;
+
+    const urlParams = new URLSearchParams(queryString);
+
+    const accessToken = urlParams.get('access_token')
+    const refreshToken = urlParams.get('refresh_token');
+    // console.log("access", accessToken, "refresh", refreshToken);
+
+
+    if (refreshToken) {
+      fetch('/refresh_token?refresh_token=' + refreshToken)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
+    }
+
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="http://localhost:8888/login"
+        >
+          Log in to Spotify
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
