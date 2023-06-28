d17c6b5a398e1d855d5027119c0641d5524c9562
ppate343 <71097041+ppate343@users.noreply.github.com>
2023-06-28 15:34:02 -0400

    button styling + profile page component

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import { GlobalStyles } from "./styles";
 import { Login } from "./pages"
 import { Profile } from './pages'
 
+
 // import './App.css';
 
 // const StyledLoginButton = styled.a`
@@ -18,6 +19,22 @@ import { Profile } from './pages'
 //   border-radius: 30px;
 //   display: inline-block;
 // `;
+
+const StyledLogoutButton = styled.button`
+  position: absolute;
+  top: 12px;
+  right: 16px;
+  padding: 4px 12px;
+  background-color: rgba(0,0,0,.7);
+  color: white;
+  font-size: 14px;
+  font-weight: 700;
+  border-radius: 30px;
+  z-index: 10;
+  @media (min-width: 768px) {
+    right: 12px;
+  }
+`;
 
 
 function App() {
@@ -46,17 +63,14 @@ function App() {
   return (
     <div className="App">
       <GlobalStyles />
-      <header className="App-header">
-        {!token ? (
-          <Login />
-        ) : (
-          <>
-            <h1>Logged in!</h1>
-            <button onClick={logout}>Log Out</button>
-            <Profile />
-          </>
-        )}
-      </header>
+      {!token ? (
+        <Login />
+      ) : (
+        <>
+          <StyledLogoutButton onClick={logout}>Log Out</StyledLogoutButton>
+          <Profile />
+        </>
+      )}
     </div>
   );
 }
