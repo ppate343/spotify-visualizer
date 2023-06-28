b1dbdd62bb2e1b2cab927fb961d5eb3cee791ca5
ppate343 <71097041+ppate343@users.noreply.github.com>
2023-06-28 15:34:12 -0400

    browser router

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from "react-dom";
 import { createRoot } from 'react-dom/client';
 import {
   createBrowserRouter,
@@ -6,6 +7,8 @@ import {
   Route,
   Link,
   useLocation,
+  BrowserRouter,
+  Routes,
 } from "react-router-dom";
 
 import './index.css';
@@ -14,35 +17,45 @@ import scrollToTop from './scrollToTop';
 import reportWebVitals from './reportWebVitals';
 
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/top-artists",
-    element: <div>top artists</div>,
-  },
-  {
-    path: "/top-tracks",
-    element: <div>top tracks</div>,
-  },
-  {
-    path: "/playlists/:id",
-    element: <div>top playlists </div>,
-  },
-  {
-    path: "/playlists",
-    element: <h1>Playlists</h1>,
-  }
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//   },
+//   {
+//     path: "/top-artists",
+//     element: <div>top artists</div>,
+//   },
+//   {
+//     path: "/top-tracks",
+//     element: <div>top tracks</div>,
+//   },
+//   {
+//     path: "/playlists/:id",
+//     element: <div>top playlists </div>,
+//   },
+//   {
+//     path: "/playlists",
+//     element: <h1>Playlists</h1>,
+//   }
+// ]);
 
 
-const domNode = document.getElementById('root');
+// const domNode = document.getElementById('root');
 
-const root = createRoot(domNode);
-root.render(<RouterProvider router={router} />)
+// const root = createRoot(domNode);
+// root.render(<RouterProvider router={router} />)
 
+
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
