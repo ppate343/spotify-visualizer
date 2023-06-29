da09d8c29a6a241795a37c3dec8395be3458b191
ppate343 <71097041+ppate343@users.noreply.github.com>
2023-06-28 22:49:13 -0400

    clean up

--- client/src/index.js
+++ client/src/index.js
@@ -14,7 +14,7 @@ import {
 import './index.css';
 import App from './App';
 import scrollToTop from './scrollToTop';
-import reportWebVitals from './reportWebVitals';
+
 
 
 // const router = createBrowserRouter([
@@ -57,7 +57,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
 
-reportWebVitals();
