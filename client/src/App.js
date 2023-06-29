a861dc33c06de38d3a933a62a81f9b448e58bf50
ppate343 <71097041+ppate343@users.noreply.github.com>
2023-06-28 22:54:57 -0400

    clean up

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,10 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
-import { accessToken, logout, getCurrentUserProfile } from './spotify';
-import { catchErrors } from './utils';
-// import styled from 'styled-components';
+import { accessToken, logout } from './spotify';
 import styled from 'styled-components/macro';
 import { GlobalStyles } from "./styles";
 import { Login } from "./pages"
 import { Profile } from './pages'
-
-
-// import './App.css';
-
-// const StyledLoginButton = styled.a`
-//   background-color: green;
-//   color: white;
-//   padding: 10px 20px;
-//   margin: 20px auto;
-//   border-radius: 30px;
-//   display: inline-block;
-// `;
 
 const StyledLogoutButton = styled.button`
   position: absolute;
@@ -40,23 +26,10 @@ const StyledLogoutButton = styled.button`
 function App() {
 
   const [token, setToken] = useState(null);
-  const [profile, setProfile] = useState(null);
 
   useEffect(() => {
 
     setToken(accessToken);
-
-    //getCurrentuserProfile returns a promise -> use a try and catch
-    const fetchData = async () => {
-      try {
-        const { data } = await getCurrentUserProfile();
-        setProfile(data);
-      } catch (e) {
-        console.error(e);
-      }
-    };
-
-    fetchData();
 
   }, []);
 
